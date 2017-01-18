9d77e672de87d3fccbd71fe6c169d8442735e585
Rosie <r.gollancz@gmail.com>
2017-01-18 10:28:34 +0000

    Adds Brie tests

--- ruby/spec/brie_aging_spec.rb
+++ ruby/spec/brie_aging_spec.rb
@@ -2,6 +2,7 @@ require 'brie_aging'
 
 describe Brie do
   let(:brie) { described_class.new("brie",0,0) }
+  let(:brie_quality_50) { described_class.new("brie",0,50) }
 
   describe "#initialize" do
     it "has a name" do
@@ -15,4 +16,15 @@ describe Brie do
     end
   end
 
+  describe "#update" do
+    it "reduces sell in by 1" do
+      expect { brie.update }.to change { brie.sell_in }.by(-1)
+    end
+    it "increases quality by 1, whilst quality is 50 or below" do
+      expect { brie.update }.to change { brie.quality }.by(1)
+    end
+    it "does not increase quality above 50" do
+      expect { brie_quality_50.update }.to_not change { brie_quality_50.quality }
+    end
+  end
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -38,20 +38,7 @@ describe GildedRose do
       gr = GildedRose.new(items)
       expect { gr.update_quality }.to change { gr.items[0].quality }.by(-2)
     end
-
-    describe "Aged brie" do
-      it "increases the quality of aged brie" do
-        items = brie
-        gr = GildedRose.new(items)
-        expect { gr.update_quality }.to change { gr.items[0].quality }.by(1)
-      end
-      it "does not increase quality above 50" do
-        items = old_brie
-        gr = GildedRose.new(items)
-        gr.update_quality
-        expect(gr.items[0].quality).to eq 50
-      end
-    end
+  end
 
     describe "Sulfuras" do
       it "quality and sell in are unaffected" do
@@ -62,21 +49,4 @@ describe GildedRose do
         expect(gr.items[0].sell_in).to eq 1
       end
     end
-
-    describe "Backstage passes" do
-      it "increase in quality when sell in is above 5 and below 10" do
-        items = backstage
-        gr = GildedRose.new(items)
-        expect { gr.update_quality }.to change { gr.items[0].quality }.by(2)
-      end
-      it "quality set to zero once concert is in the past" do
-        items = backstage
-        gr = GildedRose.new(items)
-        expect { gr.update_quality }.to change { gr.items[0].quality }.by(2)
-      end
-    end
   end
-
-
-
-end

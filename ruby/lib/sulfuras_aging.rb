771e0970086c19ca9245448665809b487aa899ab
Rosie <r.gollancz@gmail.com>
2017-01-18 10:35:54 +0000

    Remove sell in from Sulfuras and update tests

--- ruby/lib/sulfuras_aging.rb
+++ ruby/lib/sulfuras_aging.rb
@@ -1,9 +1,8 @@
 class Sulfuras
-  attr_accessor :name, :sell_in, :quality
+  attr_accessor :name, :quality
 
-  def initialize(name,sell_in,quality)
+  def initialize(name,quality)
     @name = name
-    @sell_in = sell_in
     @quality = quality
   end
 
--- ruby/spec/sulfuras_aging_spec.rb
+++ ruby/spec/sulfuras_aging_spec.rb
@@ -1,17 +1,14 @@
 require 'sulfuras_aging'
 
 describe Sulfuras do
-  let(:sulfuras) { described_class.new("sulfuras",0,0) }
+  let(:sulfuras) { described_class.new("sulfuras",80) }
 
   describe "#initialize" do
     it "has a name" do
       expect(sulfuras.name).to eq "sulfuras"
     end
-    it "has a sell_in" do
-      expect(sulfuras.sell_in).to eq 0
-    end
     it "has a quality" do
-      expect(sulfuras.quality).to eq 0
+      expect(sulfuras.quality).to eq 80
     end
   end
 end
